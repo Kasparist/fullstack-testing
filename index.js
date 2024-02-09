bd09e37fc32d16b52d0f6478234ae63d3ddd9414
Kasparist <kasper.kokkonen@edu.sasky.fi>
2024-02-09 10:25:36 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const morgan = require('morgan')
 const cors = require('cors')
 
 const requestLogger = (request, response, next) => {
@@ -16,8 +15,6 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(express.json())
-app.use(morgan('tiny'))
-app.use(morgan((':method :url :status :res[content-length] - :response-time ms :post-content')))
 app.use(requestLogger)
 app.use(cors())
 app.use(express.static('dist'))
